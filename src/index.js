28e8b5c79712bda354a0efdc30ddcbc63015e855
veselin0 <vesselin60@gmail.com>
2020-08-31 18:35:56 +0300

    practice 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+// Objective: Fill in the boilerplate React code required to render an
+// unordered list (<ul>) to the page. The list should contain 3 list items
+// (<li>) with anything in them you want.
+
+// HINTS:
+// import the libraries you need first
+// use one of the libraries to render some JSX to the page
+
 import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
     <div>
-        <h1>Hello, Gocho!</h1>
-        <p>Hello, Kochev!!</p>
+        <ul>
+            <li>Gocho</li>
+            <li>Bocho</li>
+            <li>Mocho</li>
+        </ul>
     </div>,
     document.getElementById("root")
 );
