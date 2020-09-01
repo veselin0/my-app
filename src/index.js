42587006a564cf6a4bc34519adf29b0d7833fc81
veselin0 <vesselin60@gmail.com>
2020-09-01 07:15:43 +0300

    React Functional Components

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-// Objective: Fill in the boilerplate React code required to render an
-// unordered list (<ul>) to the page. The list should contain 3 list items
-// (<li>) with anything in them you want.
-
-// HINTS:
-// import the libraries you need first
-// use one of the libraries to render some JSX to the page
-
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
+const MyApp = () => {
+    return (
     <div>
         <ul>
-            <li>Gocho</li>
-            <li>Bocho</li>
-            <li>Mocho</li>
+            <li>Guchi</li>
+            <li>Bochi</li>
+            <li>Mochi</li>
         </ul>
-    </div>,
+    </div>
+    );
+};
+
+ReactDOM.render(
+    <MyApp />,
     document.getElementById("root")
 );
