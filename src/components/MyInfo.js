95762846034b04f9a8e0a6875eb03163753e9331
Veselin <vesselin60@gmail.com>
2020-09-02 09:46:50 +0300

    Move Components into Separate Files

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function MyInfo() {
+    return (
+        <div>
+            <h1>Gocho The Prophet</h1>
+            <p>Born in the center of the Universe (Madgerito)</p>
+            <ol>
+                <li>Thaiti</li>
+                <li>Thailand</li>
+                <li>Thaiwan</li>
+            </ol>
+        </div>
+    );
+};
+
+//export default MyInfo;
